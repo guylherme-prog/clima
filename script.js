78bf93d45d92ca4ade308d08244a94ed0a9df4bb
guylherme-prog <guylhermebezerra97@gmail.com>
2025-09-22 11:49:57 -0300

    clima
    Clima

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script do Weather Widget
+const script = document.createElement('script');
+script.src = "https://app3.weatherwidget.org/js/?id=ww_9cca64404b2aa";
+script.async = true;
+document.body.appendChild(script);
